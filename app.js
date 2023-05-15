75de411d80eff43c6d01c4a96119efda3912c7b1
Darshan614 <jaindarshan614@gmail.com>
2023-05-15 11:39:38 +0530

    bug rem

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 require("dotenv").config();
-app.use('cors');
+app.use(cors());
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
